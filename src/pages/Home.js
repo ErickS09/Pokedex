53f5e0543e6d4a53cad5e3e88d9bda27c679ea28
ErickS09 <josiels88@gmail.com>
2022-01-06 12:48:26 -0600

    Se installa antdesing y se remplaza pokemon item

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react';
 import { getPokemonList } from '../services'
 import PokemonItem from '../components/PokemonItem';
+import { List, Avatar } from 'antd';
+import { Link } from 'react-router-dom';
 
 
 function Home() {
@@ -11,11 +13,19 @@ function Home() {
     }, [])
 
     return (
-        <>
-            {state.map((pokemon, i) => (
-                <PokemonItem key={`pokemon-${i}`} name={pokemon.name} urlInfo={pokemon.url} />
-            ))}
-        </>
+        <List
+        itemLayout="horizontal"
+        dataSource={state}
+        renderItem={pokemon => (
+            <List.Item>
+              <List.Item.Meta
+                avatar={<Avatar src="https://fiverr-res.cloudinary.com/images/q_auto,f_auto/gigs/133704119/original/798ab218286c66085499f6e6c9e705b209e2b11c/answer-all-of-pokemon-questions.png" />}
+                title={<Link to={`/detail/${pokemon.name}`}>{pokemon.url}</Link>}
+                description={pokemon.name}
+              />
+            </List.Item>
+          )}
+        />
     )
 }
 export default Home
