ec8aa5035de3daf70373d46a347bdc442747d44e
ErickS09 <josiels88@gmail.com>
2022-01-07 09:29:40 -0600

    dev

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,14 +1,14 @@
-
 import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { getPokemonByName, getPokemonList } from '../services'
+import { Card } from 'antd';
 
 function Detail() {
 
     const params = useParams()
     const { pokemonName } = params
     const [state, setState] = useState({})
-
+    const { Meta } = Card;
 
     useEffect(() => {
         getPokemonByName(pokemonName).then((data) => setState(data))
@@ -18,17 +18,21 @@ function Detail() {
 
     return (
         <div>
-            <h2>
-                {pokemonName.toUpperCase()}
-            </h2>
-            {state?.id ? <img src={state.sprites.front_shiny} /> : null}
-            {state?.id ? <img src={state.sprites.front_default} /> : null}
+            <Card
+                hoverable
+                style={{ width: 240, margin: 15 }}
+               
+                cover={state?.id ? <img src={state.sprites.front_shiny} /> : null}
+            >
+                <Meta title={pokemonName.toUpperCase()} />
+                <h3>Experiencia: {state.base_experience}</h3>
+            </Card>
 
-            <h3>Experiencia: {state.base_experience}</h3>
-            <h3>peso: {state.weight}</h3>
-            
-            
+
         </div>
     )
 }
 export default Detail
+
+
+
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,6 +7,7 @@ import { Link } from 'react-router-dom';
 function Home() {
     const [state, setState] = useState([])
     
+    
     useEffect(() => {
         getPokemonList().then((data) => setState(data.results))
     }, [])
